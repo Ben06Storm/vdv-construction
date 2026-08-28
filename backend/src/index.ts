7fe90fc89c97b6504c98d6593e8ba06f2f2d02c2
Ben06Storm <veneavatamaniuc@gmail.com>
2026-08-28 17:30:25 +0300

    security: configure cors and env example

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,10 +5,14 @@ import reviewsRouter from './routes/reviews';
 import adminRouter from './routes/admin';
 
 const app = express();
-
 const PORT = process.env.PORT || 5000;
+const FRONTEND_URL = process.env.FRONTEND_URL;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: FRONTEND_URL,
+  }),
+);
 app.use(express.json());
 app.use('/reviews', reviewsRouter);
 app.use('/admin', adminRouter);
